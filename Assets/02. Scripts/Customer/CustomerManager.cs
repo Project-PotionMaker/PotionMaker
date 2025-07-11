0ded3d45141f22f25038c2258428a529ace474f2
BBTibbers <youngsuk0311@gmail.com>
2025-07-11 18:23:40 +0900

    Temp | 7/11

---
+++ Assets/02. Scripts/Customer/CustomerManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CustomerManager : MonoBehaviour
+{
+    //Customer 스폰 및 초기화는 ObjectPooling구현 이후
+
+    [SerializeField]
+    private int _lostCustomerCount;
+    public int LostCustomerCount { get => _lostCustomerCount; set => _lostCustomerCount = value; }
+
+    private const int MAX_CUSTOMER_LOST = 5; 
+    public void AddLostCustomer()
+    {
+        _lostCustomerCount++;
+        if(_lostCustomerCount >= MAX_CUSTOMER_LOST)
+        {
+            PhaseManager.Instance.TransitionPhase(EPhaseType.EndingPhase);
+        }
+    }
+}

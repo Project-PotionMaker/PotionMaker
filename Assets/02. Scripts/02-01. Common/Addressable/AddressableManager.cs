d056b4c9ebe75f25cf51a681fdc07c8aedc48a59
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-07-14 16:59:41 +0900

    Feat | 폴더명 및 라벨명 기반 Addressable 리소스 로딩 메서드 구현

--- Assets/02. Scripts/02-01. Common/Addressable/AddressableManager.cs
+++ Assets/02. Scripts/02-01. Common/Addressable/AddressableManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.AddressableAssets;
 using UnityEngine;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 public class AddressableManager : MonoBehaviourSingleton<AddressableManager>
@@ -16,5 +17,33 @@ public class AddressableManager : MonoBehaviourSingleton<AddressableManager>
         GameObject prefab = await handle.Task;
         Debug.Log($"원격 저장소에서 로드한 프리팹명 : {prefab.name}");
         return prefab;
+    }
+
+    public async Task<List<GameObject>> LoadPrefabsByFolder(string folderKey)
+    {
+        List<GameObject> result = new List<GameObject>();
+        AsyncOperationHandle<IList<GameObject>> handle = Addressables.LoadAssetsAsync<GameObject>(folderKey, null);
+        IList<GameObject> prefabs = await handle.Task;
+
+        foreach (var prefab in prefabs)
+        {
+            Debug.Log($"업로드된 폴더[{folderKey}]에서 로드한 프리팹명 : {prefab.name}");
+            result.Add(prefab);
+        }
+        return result;
+    }
+
+    public async Task<List<GameObject>> LoadPrefabsByLabel(string label)
+    {
+        List<GameObject> result = new List<GameObject>();
+        AsyncOperationHandle<IList<GameObject>> handle = Addressables.LoadAssetsAsync<GameObject>(label, null);
+        IList<GameObject> prefabs = await handle.Task;
+
+        foreach (var prefab in prefabs)
+        {
+            Debug.Log($"라벨[{label}]로 로드한 프리팹명 : {prefab.name}");
+            result.Add(prefab);
+        }
+        return result;
     }
 }

18901c6c80002b5da024830241687ad2c35573ec
wooing26 <104512612+wooing26@users.noreply.github.com>
2025-07-16 18:05:37 +0900

    Fix | Player Move Ability 인풋 구분 수정
    온리안을 위한 로직 추가

--- Assets/02. Scripts/02-03. Player/Ability/PlayerMoveAbility.cs
+++ Assets/02. Scripts/02-03. Player/Ability/PlayerMoveAbility.cs
@@ -13,6 +13,11 @@ public class PlayerMoveAbility : PlayerAbility
 
     private void FixedUpdate()
     {
+        if (!_photonView.IsMine)
+        {
+            return;
+        }
+
         Vector2 moveInput = InputManager.Instance.MoveInput;
 
         _rigidbody.linearVelocity = new Vector3(moveInput.x, 0, moveInput.y) * _owner.MoveSpeed;

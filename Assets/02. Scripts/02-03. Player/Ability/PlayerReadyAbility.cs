9448ee1a4f235188b5b8ec2f430b843337bbbca8
wooing26 <104512612+wooing26@users.noreply.github.com>
2025-07-16 17:42:02 +0900

    Feat | Player Ready 구현
    Ready 집계 시스템이 구현되면 완성 예정

---
+++ Assets/02. Scripts/02-03. Player/Ability/PlayerReadyAbility.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerReadyAbility : PlayerAbility
+{
+    private bool _isReady = false;
+
+    private void Start()
+    {
+        InputManager.Instance.OnReadyEvent += Ready;
+    }
+
+    private void Ready()
+    {
+        _isReady = !_isReady;
+        Debug.Log($"Ready : {_isReady}");
+    }
+}

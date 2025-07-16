81c4c9f365f964fd274cb9b152f9124cc8ca5b37
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-07-16 14:08:11 +0900

    Feat | 재료 및 결과물 클래스 설계

--- Assets/02. Scripts/02-04. Potion/Ingredient/Ingredient.cs
+++ Assets/02. Scripts/02-04. Potion/Ingredient/Ingredient.cs
@@ -1,19 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
+using VInspector;
 
 public class Ingredient : MonoBehaviour
 {
     private IngredientData _data;
     public IngredientData Data => _data;
-
-
-    private void Awake()
-    {
-
-    }
-
-    public void Init(IngredientData data)
-    {
-        _data = data;
-    }
-
 }
--- Assets/02. Scripts/02-04. Potion/Output/Output.cs
+++ Assets/02. Scripts/02-04. Potion/Output/Output.cs
@@ -1,22 +1,61 @@
+using System.Collections.Generic;
 using UnityEngine;
+using VInspector;
 
 public class Output : MonoBehaviour
 {
-    private OutputData _data;
-    public OutputData Data => _data;
+    private EInputType _currentInputType;
+    public EInputType CurrentInputType => _currentInputType;
 
-    private EInputType _state;
-    public EInputType State => _state;
+    private OutputData _outputData;
+    public OutputData OutputData => _outputData;
+
+    private PotionData _potionData = null;
+    public PotionData PotionData => _potionData;
+
+    private string _outputCode;
+    public string OutputCode => _outputCode;
+
+    private MeshFilter _meshFilter;
+
+
+    [Foldout("Project")]
+    [SerializeField]
+    private List<(EInputType, Mesh)> _meshList;
+
+    private Dictionary<EInputType, Mesh> _meshDict;
 
     private void Awake()
     {
-        
+        Init();
     }
 
-    public void Init(OutputData data)
+    private void Init()
     {
-        _data = data;
+        _meshDict = new Dictionary<EInputType, Mesh>();
+        foreach (var meshInfo in _meshList)
+        {
+            _meshDict.Add(meshInfo.Item1, meshInfo.Item2);
+        }
+
+        _meshFilter = GetComponent<MeshFilter>();
     }
 
+    // 병입기를 제외한 모든 조리기구에서 조리가 완료되었을 때 호출해야하는 메서드
+    public void ChangeState(EInputType newInputType, OutputData newData, string newOutputCode)
+    {
+        _currentInputType = newInputType;
+        _outputData = newData;
+        _outputCode = newOutputCode;
+        _meshFilter.mesh = _meshDict[newInputType];
+    }
 
+    // 병입기에서 조리가 완료되었을 때 호출해야하는 메서드
+    public void CreatePotion(PotionData potionData, Mesh potionMesh)
+    {
+        _currentInputType = EInputType.BottlerOutput;
+        _outputData = null;
+        _potionData = potionData;
+        _meshFilter.mesh = potionMesh;
+    }
 }

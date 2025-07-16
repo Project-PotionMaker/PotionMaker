db74eec1633d466c50901ddd2ccc5b6619f56c8b
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-07-16 18:15:43 +0900

    Feat | OutputManager 구현

--- Assets/02. Scripts/02-04. Potion/Output/OutputManager.cs
+++ Assets/02. Scripts/02-04. Potion/Output/OutputManager.cs
@@ -5,8 +5,70 @@ using UnityEngine;
 
 public class OutputManager : MonoBehaviourSingleton<OutputManager>
 {
-    public GameObject CreateOutput(List<int> TIDList, EInputType type)
+    [SerializeField]
+    private GameObject _failureOutput;
+
+    private Dictionary<string, int> _potionDataDict;
+
+    private RecipeCodeHandler _recipeCodeHandler;
+    private RecipeCodeVerifier _recipeCodeVerifier;
+
+    protected override void Awake()
     {
-        return null;
+        base.Awake();
+        Init();
+    }
+
+    private void Init()
+    {
+        var potionDataList = DataTable.Instance.GetPotionDataList();
+        foreach (var potionData in potionDataList)
+        {
+            _potionDataDict.Add(potionData.RecipeCode, potionData.TID);
+        }
+
+        _recipeCodeHandler = new RecipeCodeHandler();
+        _recipeCodeVerifier = new RecipeCodeVerifier(potionDataList);
+    }
+
+    public GameObject TryCreateOutput(List<int> TIDList, int machineTID, EInputType type, Vector3 machinePosition)
+    {
+        string recipeCode;
+        if (type == EInputType.HeatingPotOutput)
+        {
+            recipeCode = _recipeCodeHandler.GenerateNumberPartCode(
+                DataTable.Instance.GetOutputData(TIDList[0]).RecipeCode,
+                DataTable.Instance.GetOutputData(TIDList[1]).RecipeCode);
+        }
+        else
+        {
+            recipeCode = DataTable.Instance.GetOutputData(TIDList[0]).RecipeCode;
+        }
+
+        if (_recipeCodeVerifier.IsValidProcess(recipeCode))
+        {
+            string newRecipeCode = _recipeCodeHandler.AddMachineCode(recipeCode, machineTID);
+
+            return OutputFactory.Instance.Create(type, machinePosition, Quaternion.identity);
+        }
+        return CreateFailureOutput(machinePosition);
+    }
+
+    public GameObject TryCreatePotion(List<int> TIDList, int bottlerTID, Vector3 machinePosition)
+    {
+        string recipeCode = 
+            _recipeCodeHandler.AddMachineCode(DataTable.Instance.GetOutputData(TIDList[0]).RecipeCode, bottlerTID);
+
+        if (_recipeCodeVerifier.IsValidPotion(recipeCode))
+        {
+            string newRecipeCode = _recipeCodeHandler.AddMachineCode(recipeCode, bottlerTID);
+            return OutputFactory.Instance.Create(EInputType.Potion, machinePosition, Quaternion.identity);
+        }
+        return CreateFailureOutput(machinePosition);
+    }
+
+    private GameObject CreateFailureOutput(Vector3 machinePosition)
+    {
+        return OutputFactory.Instance.Create(EInputType.FailureOutput, machinePosition, Quaternion.identity);
     }
 }

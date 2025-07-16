1eb0e4272d7c1836981dbda9b0be8116e7905e99
wooing26 <104512612+wooing26@users.noreply.github.com>
2025-07-16 16:48:20 +0900

    Feat | Player 이동 구현
    InputManager를 통한 Player 이동 구현

---
+++ Assets/02. Scripts/02-03. Player/Ability/PlayerMoveAbility.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerMoveAbility : PlayerAbility
+{
+    private Rigidbody _rigidbody;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _rigidbody = _owner.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 moveInput = InputManager.Instance.MoveInput;
+
+        _rigidbody.linearVelocity = new Vector3(moveInput.x, 0, moveInput.y) * _owner.MoveSpeed;
+    }
+}

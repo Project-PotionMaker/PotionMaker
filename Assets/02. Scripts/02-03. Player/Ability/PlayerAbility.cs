e45a2f8f309048984f619a0642ed80e85a4d7561
wooing26 <104512612+wooing26@users.noreply.github.com>
2025-07-16 18:06:58 +0900

    Fix | Player Ability에 PhotonView 추가

--- Assets/02. Scripts/02-03. Player/Ability/PlayerAbility.cs
+++ Assets/02. Scripts/02-03. Player/Ability/PlayerAbility.cs
@@ -1,11 +1,14 @@
+using Photon.Pun;
 using UnityEngine;
 
 public class PlayerAbility : MonoBehaviour
 {
     protected Player _owner { get; private set; }
+    protected PhotonView _photonView { get; private set; }
     
     protected virtual void Awake()
     {
         _owner = GetComponentInParent<Player>();
+        _photonView = GetComponentInParent<PhotonView>();
     }
 }

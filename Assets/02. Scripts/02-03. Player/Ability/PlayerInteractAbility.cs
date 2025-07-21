4d5289f995f67b409272ee2891dcd4fcb4549d4a
wooing26 <104512612+wooing26@users.noreply.github.com>
2025-07-21 14:31:24 +0900

    Feat | Player Animation 구현
    Player Animator, Idle, Move Animation 추가

--- Assets/02. Scripts/02-03. Player/Ability/PlayerInteractAbility.cs
+++ Assets/02. Scripts/02-03. Player/Ability/PlayerInteractAbility.cs
@@ -39,6 +39,11 @@ public class PlayerInteractAbility : PlayerAbility
     {
         Vector3 targetPosition = _owner.GetFrontPosition();
         GameObject structure = GridManager.Instance.GetObjectOnGrid(targetPosition);
+        if (ReferenceEquals(structure, null))
+        {
+            return;
+        }
+
         IGridItemHandler itemHandler = structure.GetComponent<IGridItemHandler>();
         if (ReferenceEquals(itemHandler, null) == false)
         {

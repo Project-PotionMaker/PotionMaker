6edee0df60f5627a8dc116cfa163d557b6a7a55d
SangJin Lee <56784814+sangjin256@users.noreply.github.com>
2025-07-22 12:55:47 +0900

    Update Assets/02. Scripts/02-05. Grid/1. Domain/Placement.cs
    Co-authored-by: Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>

--- Assets/02. Scripts/02-05. Grid/1. Domain/Placement.cs
+++ Assets/02. Scripts/02-05. Grid/1. Domain/Placement.cs
@@ -8,8 +8,7 @@ public class Placement
     public List<Vector3Int> OccupiedPositionList;
     public int TID { get; private set; }
     public EStructureType structureType { get; private set; }
-    public GameObject structureObject { get; private set; }
-
+    public GameObject StructureObject { get; private set; }
     public Placement(List<Vector3Int> occupiedPositionList, int tid, EStructureType type, GameObject structureObject)
     {
         if (tid < 10000)

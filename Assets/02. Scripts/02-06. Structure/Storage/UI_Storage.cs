a00a4202e39ea84ebcb0fd003f3618bcd38af3ed
SangJin Lee <56784814+sangjin256@users.noreply.github.com>
2025-07-22 13:16:43 +0900

    Feat | Structure들 UI추가

---
+++ Assets/02. Scripts/02-06. Structure/Storage/UI_Storage.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Runtime.CompilerServices;
+using TMPro;
+using UnityEngine;
+
+public class UI_Storage : MonoBehaviour
+{
+    [SerializeField]
+    private Storage _storage;
+    [SerializeField]
+    private TextMeshProUGUI _nameTextUI;
+
+    private void Start()
+    {
+        _storage.OnDataChanged += InitUIStorage;
+    }
+
+    private void InitUIStorage(StorageStat stat)
+    {
+        _nameTextUI.text = DataTable.Instance.GetIngredientData(stat.IngredientTID).Name;
+    }
+
+    private void Update()
+    {
+        transform.forward = Camera.main.transform.forward;
+    }
+}

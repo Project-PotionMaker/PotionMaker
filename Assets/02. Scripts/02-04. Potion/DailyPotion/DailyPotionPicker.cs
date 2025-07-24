0eb9843f8611c30085a283445acf98c54173d312
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-07-25 08:58:15 +0900

    Fix | 포션 중복픽 되지않도록 수정 및 Dictionary 사용구조로 변경

--- Assets/02. Scripts/02-04. Potion/DailyPotion/DailyPotionPicker.cs
+++ Assets/02. Scripts/02-04. Potion/DailyPotion/DailyPotionPicker.cs
@@ -2,10 +2,21 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class PotionPickInfo
+{
+    public PotionData Data;
+    public bool IsPicked;
+
+    public PotionPickInfo(PotionData data)
+    {
+        Data = data;
+        IsPicked = false;
+    }
+}
+
 public class DailyPotionPicker
 {
-    // [티어][포션 인덱스]
-    private List<List<PotionData>> _potionDataList;
+    private Dictionary<ETierType, List<PotionPickInfo>> _potionDataDict = new Dictionary<ETierType, List<PotionPickInfo>>();
 
     public DailyPotionPicker()
     {
@@ -15,20 +26,22 @@ public class DailyPotionPicker
     private void InitDailyPotionPicker()
     {
         int tierCount = Enum.GetValues(typeof(ETierType)).Length;
-        _potionDataList = new List<List<PotionData>>(tierCount + 1);
-        InitTierPotionList(tierCount);
+        InitTierPotionDict(tierCount);
     }
 
-    private void InitTierPotionList(int maxTier)
+    private void InitTierPotionDict(int maxTier)
     {
         var potionDataList = DataTable.Instance.GetPotionDataList();
-        for (int currentTier = 1; currentTier <= maxTier; currentTier++)
+        for (int currentTier = 0; currentTier < maxTier; currentTier++)
         {
+            var tier = (ETierType)currentTier;
+            _potionDataDict.Add(tier, new List<PotionPickInfo>());
+
             foreach (var potionData in potionDataList)
             {
                 if (potionData.Tier == currentTier)
                 {
-                    _potionDataList[currentTier].Add(potionData);
+                    _potionDataDict[tier].Add(new PotionPickInfo(potionData));
                 }
             }
         }
@@ -36,28 +49,56 @@ public class DailyPotionPicker
 
     public List<PotionData> PickDailyPotion(List<int> potionTierList)
     {
+        ResetIsPicked();
         List<PotionData> dailyPotionDataList = new List<PotionData>();
+        
         foreach (int potionTier in potionTierList)
         {
-            dailyPotionDataList.Add(PickPotion(potionTier));
+            if (potionTier <= 0 || Enum.GetValues(typeof(ETierType)).Length <= potionTier)
+            {
+                Debug.LogWarning($"올바르지 않은 티어 값입니다. 1 ~ 3 사이의 티어값인지 확인해주세요.");
+            }
+            dailyPotionDataList.Add(PickPotion((ETierType)potionTier - 1));
         }
         return dailyPotionDataList;
     }
 
     public List<PotionData> PickDailyPotion(List<ETierType> potionTierList)
     {
+        ResetIsPicked();
         List<PotionData> dailyPotionDataList = new List<PotionData>();
         foreach (ETierType potionTier in potionTierList)
         {
-            dailyPotionDataList.Add(PickPotion((int)potionTier + 1));
+            dailyPotionDataList.Add(PickPotion(potionTier));
         }
         return dailyPotionDataList;
     }
 
-
-    private PotionData PickPotion(int tier)
+    private PotionData PickPotion(ETierType tier)
     {
-        int randomPotionIndex = UnityEngine.Random.Range(0, _potionDataList[tier].Count);
-        return _potionDataList[tier][randomPotionIndex];
+        var potionList = _potionDataDict[tier];
+        while (true)
+        {
+            int randomIndex = UnityEngine.Random.Range(0, potionList.Count);
+            var pickInfo = potionList[randomIndex];
+
+            if (!pickInfo.IsPicked)
+            {
+                pickInfo.IsPicked = true;
+                return pickInfo.Data;
+            }
+        }
+    }
+
+    private void ResetIsPicked()
+    {
+        foreach (var keyValuePair in _potionDataDict)
+        {
+            var potionPickInfoList = keyValuePair.Value;
+            foreach (var pickInfo in potionPickInfoList)
+            {
+                pickInfo.IsPicked = false;
+            }
+        }
     }
 }

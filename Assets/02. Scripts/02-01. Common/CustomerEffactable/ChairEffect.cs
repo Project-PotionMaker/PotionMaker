b9851f15c90c370b034b3c6db88284ed8908ec3e
BBTibbers <youngsuk0311@gmail.com>
2025-07-25 10:21:34 +0900

    Feat | 의자 2종 구현

---
+++ Assets/02. Scripts/02-01. Common/CustomerEffactable/ChairEffect.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ChairEffect : ICustomerEffectable<Furniture, FurnitureStat>
+{
+    public void Effect(Furniture furniture, FurnitureStat stat, Customer customer)
+    {
+        customer.CustomerEndurance.LoseEnduranceSpeed *= stat.Data.EffectRate;
+    }
+}

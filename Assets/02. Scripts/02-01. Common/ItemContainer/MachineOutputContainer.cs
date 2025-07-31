37af50da22601885c88fc7533ec413c5c124c364
SangJin Lee <sangjin256@naver.com>
2025-08-01 03:27:42 +0900

    Feat | StructureManager 정상화중

--- Assets/02. Scripts/02-01. Common/ItemContainer/MachineOutputContainer.cs
+++ Assets/02. Scripts/02-01. Common/ItemContainer/MachineOutputContainer.cs
@@ -1,4 +1,3 @@
-using Photon.Pun;
 using System.Linq;
 using UnityEngine;
 
--- Assets/02. Scripts/02-03. Currency/3.Manager/CurrencyManager.cs
+++ Assets/02. Scripts/02-03. Currency/3.Manager/CurrencyManager.cs
@@ -3,44 +3,13 @@ using Mirror;
 using System;
 using System.Diagnostics;
 
-public class CurrencyManager : NetworkBehaviour
+public class CurrencyManager : NetworkBehaviourSingleton<CurrencyManager>
 {
     public event Action OnDataChanged;
     public static Action OnInitialized;
 
     private Currency _coin;
     public CurrencyDTO Coin => _coin.ToDTO();
-
-    private static CurrencyManager _instance;
-    public static CurrencyManager Instance
-    {
-        get
-        {
-            if (_instance == null)
-            {
-                // 매우 위험한 로직. Awake가 이미 호출되어 있어야 함.
-                // 만약 이 시점에 _instance가 null이라면, 네트워크 관련 초기화가 되지 않았을 가능성이 높습니다.
-                // 따라서 이 프로퍼티는 'Awake에서 초기화된다'는 가정 하에 사용해야 합니다.
-                UnityEngine.Debug.LogError("CurrencyManager.Instance가 아직 초기화되지 않았습니다. Awake() 또는 OnStartClient()가 호출될 때까지 기다리세요.");
-            }
-            return _instance;
-        }
-    }
-
-    private void Awake()
-    {
-        if (_instance == null)
-        {
-            _instance = this;
-            DontDestroyOnLoad(this); // 씬 전환 시에도 유지
-            UnityEngine.Debug.Log("CurrencyManager 인스턴스 초기화 완료 (Awake).");
-        }
-        else if (_instance != this)
-        {
-            UnityEngine.Debug.LogWarning("중복된 CurrencyManager 인스턴스 발견. 파괴합니다.");
-            Destroy(gameObject);
-        }
-    }
 
     public override void OnStartServer()
     {

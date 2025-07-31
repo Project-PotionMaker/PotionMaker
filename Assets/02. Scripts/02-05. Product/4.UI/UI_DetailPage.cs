5696c2b7449dd9c9fb1d1acf579997a3d1ceaa00
pinehj <pinehj00@gmail.com>
2025-07-31 09:57:06 +0900

    Feat | 상점 디테일페이지/재화 연동

--- Assets/02. Scripts/02-05. Product/4.UI/UI_DetailPage.cs
+++ Assets/02. Scripts/02-05. Product/4.UI/UI_DetailPage.cs
@@ -1,28 +1,29 @@
-using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI_ProductSlot:MonoBehaviour
+public class UI_DetailPage : MonoBehaviour
 {
-    public event Action<ProductDTO> OnSlotClicked;
-
     [SerializeField]
     private Image _productImage;
     [SerializeField]
     private TextMeshProUGUI _productNameTextUI;
     [SerializeField]
+    private TextMeshProUGUI _productDescriptionTextUI;
+    [SerializeField]
     private TextMeshProUGUI _productPriceTextUI;
     [SerializeField]
-    private Button _slotButton;
+    private Button _buyButton;
 
+    
     public void Refresh(ProductDTO productDTO)
     {
+        //_productImage
         _productNameTextUI.text = productDTO.Data.Name;
+        //_productDescriptionTextUI.text = productDTO.Data.
         _productPriceTextUI.text = productDTO.Data.Price.ToString("N0");
-        _slotButton.onClick.RemoveAllListeners();
-        _slotButton.onClick.AddListener(() => OnSlotClicked?.Invoke(productDTO));
 
-        //_slotButton.onClick.AddListener(() => ProductManager.Instance.RequestBuy(productDTO.Data.ProductType, productDTO.Data.TID));
+        _buyButton.onClick.RemoveAllListeners();
+        _buyButton.onClick.AddListener(() => { ProductManager.Instance.RequestBuy(productDTO.Data.ProductType, productDTO.Data.TID); });
     }
 }

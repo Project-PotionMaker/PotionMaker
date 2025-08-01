320012d724f5f32e4bab2437ebf93a17177a316e
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-08-01 17:00:45 +0900

    Fix | 열거형 데이터 테이블 변경에 따른 판매 시스템 수정

--- Assets/02. Scripts/02.-04. Sales/4.UI/UI_SalesVolumeSlot.cs
+++ Assets/02. Scripts/02.-04. Sales/4.UI/UI_SalesVolumeSlot.cs
@@ -8,16 +8,16 @@ public class UI_SalesVolumeSlot : MonoBehaviour
     private Image _potionImage;
     [SerializeField]
     private TextMeshProUGUI _salesVolumeTextUI;
-    public void Refresh(EPotionType potionType, bool isTotal)
+    public void Refresh(int potionTID, bool isTotal)
     {
         // Todo: 포션 이미지
         if (isTotal)
         {
-            _salesVolumeTextUI.text = SalesManager.Instance.Sales.TotalSalesVolumeDict[potionType].ToString("N0");
+            _salesVolumeTextUI.text = SalesManager.Instance.Sales.TotalSalesVolumeDict[potionTID].ToString("N0");
         }
         else
         {
-            _salesVolumeTextUI.text = SalesManager.Instance.Sales.DailySalesVolumeDict[potionType].ToString("N0");
+            _salesVolumeTextUI.text = SalesManager.Instance.Sales.DailySalesVolumeDict[potionTID].ToString("N0");
         }
     }
 }

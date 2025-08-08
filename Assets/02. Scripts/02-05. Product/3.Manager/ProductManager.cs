2cfbca5c522470263a6951d9f05fbc79134217d9
pinehj <pinehj00@gmail.com>
2025-08-08 14:26:17 +0900

    Refactor | 불필요한 네임스페이스 제거

--- Assets/02. Scripts/02-05. Product/3.Manager/ProductManager.cs
+++ Assets/02. Scripts/02-05. Product/3.Manager/ProductManager.cs
@@ -1,11 +1,7 @@
 using Mirror;
-using Mirror.BouncyCastle.Math.Field;
-
-//using Photon.Pun;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 

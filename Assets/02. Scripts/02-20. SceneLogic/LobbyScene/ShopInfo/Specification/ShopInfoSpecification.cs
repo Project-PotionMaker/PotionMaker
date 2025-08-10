8f596ae2cfbbaf770ce5fb529ff3496aae59f942
Mingyu0626 <mg010626@naver.com>
2025-08-10 17:14:45 +0900

    Feat | ShopInfo 명세 구현

---
+++ Assets/02. Scripts/02-20. SceneLogic/LobbyScene/ShopInfo/Specification/ShopInfoSpecification.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using static UnityEngine.Rendering.DebugUI;
+
+public class ShopInfoSpecification
+{
+    private static readonly Regex ShopNameRegex =
+        new Regex(@"^[0-9가-힣a-zA-Z]{2,15}$", RegexOptions.Compiled);
+
+    public string ErrorMessage { get; private set; }
+
+    public bool IsSatisfied(string shopName, Currency currency, Reputation reputation, int day)
+    {
+        if (string.IsNullOrEmpty(shopName))
+        {
+            ErrorMessage = "포션 상점명은 비어있을 수 없습니다.";
+            return false;
+        }
+
+        if (!ShopNameRegex.IsMatch(shopName))
+        {
+            ErrorMessage = "포션 상점명은 2자 이상 15자 이하의 한글 또는 영문이어야 합니다.";
+            return false;
+        }
+
+        if (day < 0)
+        {
+            ErrorMessage = "진행일 수는 0보다 커야 합니다.";
+            return false;
+        }
+        return true;
+    }
+}

6e45ebdd2b3c41e6c4cf4ed0040888c49476b984
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-08-12 13:19:08 +0900

    Refactor | 정적 클래스를 이용한 옵저버 패턴이 아닌 인터페이스 기반 로직으로 개선

--- Assets/02. Scripts/02-20. SceneLogic/LobbyScene/ShopInfo/3. Manager/ShopInfoManager.cs
+++ Assets/02. Scripts/02-20. SceneLogic/LobbyScene/ShopInfo/3. Manager/ShopInfoManager.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class ShopInfoManager : MonoBehaviourSingleton<ShopInfoManager>
+public class ShopInfoManager : NetworkBehaviourSingleton<ShopInfoManager>
 {
     private ShopInfo _shopInfo;
     public ShopInfo ShopInfo
@@ -11,11 +13,25 @@ public class ShopInfoManager : MonoBehaviourSingleton<ShopInfoManager>
 
     private ShopInfoRepository _shopInfoRepository;
 
+    private List<IShopInfoSaveable> _shopInfoSaveableList = new();
+
     protected override void Awake()
     {
         base.Awake();
     }
 
+    private void FindAllShopInfoSaveables()
+    {
+        var shopInfoSaveableEnumerator =
+            FindObjectsByType<MonoBehaviour>(FindObjectsSortMode.None).OfType<IShopInfoSaveable>();
+
+        foreach (var shopInfoSaveable in shopInfoSaveableEnumerator)
+        {
+            _shopInfoSaveableList.Add(shopInfoSaveable);
+        }
+    }
+
+    // LobbyScene에서 MakeRoom(방 생성) 버튼을 눌렀을 때 호출해줘야 한다.
     public void InitShopInfoManager(ShopInfo shopInfo)
     {
         if (!ReferenceEquals(shopInfo, null))
@@ -25,9 +41,20 @@ public class ShopInfoManager : MonoBehaviourSingleton<ShopInfoManager>
         _shopInfoRepository = new ShopInfoRepository();
     }
 
+    public void ApplyShopInfo()
+    {
+        foreach (var shopInfoSaveable in _shopInfoSaveableList)
+        {
+            shopInfoSaveable.ApplyLoadedData(_shopInfo);
+        }
+    }
+
     public void SaveShopInfo()
     {
-        ShopInfoSaveEvents.TriggerSaveRequest(_shopInfo);
+        foreach (var shopInfoSaveable in _shopInfoSaveableList)
+        {
+            shopInfoSaveable.ProvideSaveData(_shopInfo);
+        }
         _shopInfoRepository.Save(_shopInfo);
     }
 

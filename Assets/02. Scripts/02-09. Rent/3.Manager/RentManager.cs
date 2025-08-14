c8ae9d2a53cd835bbd111913dfff591f1189cdfa
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-08-14 12:50:36 +0900

    Remove | 로그 출력문 제거

--- Assets/02. Scripts/02-09. Rent/3.Manager/RentManager.cs
+++ Assets/02. Scripts/02-09. Rent/3.Manager/RentManager.cs
@@ -21,7 +21,6 @@ public class RentManager : NetworkBehaviourSingleton<RentManager>, IShopInfoSave
         }
 
         _rent = ShopInfoManager.Instance.ShopInfo.Rent;
-        Debug.Log(_rent.CurrentRentCost);
         //// 레이아웃 데이터를 들고 있는 매니저로부터 레이아웃 TID 가져옴
         //LayoutData data = DataTable.Instance.GetLayoutData(10000);
         //_rent = new Rent(1, data.InitialRentCost, data.RentIncrement);

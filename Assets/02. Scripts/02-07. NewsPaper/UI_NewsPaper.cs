d8c4f535c9e5c74c8d03bff1d58ad5cfc0988bb5
SangJin Lee <sangjin256@naver.com>
2025-08-17 03:53:40 +0900

    FEAT | 뉴스 UI 폴리싱

--- Assets/02. Scripts/02-07. NewsPaper/UI_NewsPaper.cs
+++ Assets/02. Scripts/02-07. NewsPaper/UI_NewsPaper.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class UI_NewsPaper : MonoBehaviour
@@ -16,18 +17,22 @@ public class UI_NewsPaper : MonoBehaviour
     private void Refresh(List<PotionData> dailyPotionDataList)
     {
         int dailyPotionListSize = dailyPotionDataList.Count;
+        GameSceneUIManager.Instance?.OpenNewspaperPopup();
+
         for (int i = 0; i < _slotDailyPotionList.Count; i++)
         {
             if (i < dailyPotionListSize)
             {
                 _slotDailyPotionList[i].gameObject.SetActive(true);
                 _slotDailyPotionList[i].RefreshSlot(dailyPotionDataList[i]);
+                _slotDailyPotionList[i].transform.DOScale(Vector3.one, 0.5f).From(Vector3.one * 1.2f)
+                    .SetEase(Ease.InOutQuad)
+                    .SetRelative(true);
             }
             else
             {
                 _slotDailyPotionList[i].gameObject.SetActive(false);
             }
         }
-        GameSceneUIManager.Instance?.OpenNewspaperPopup();
     }
 }

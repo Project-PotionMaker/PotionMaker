d693002ea46f6ac0da3be26093ff72b474af28fe
Mingyu0626 <mg010626@naver.com>
2025-08-17 13:40:42 +0900

    Feat | 영업 준비 페이즈 진입 사운드 신문 Refresh에서 재생하도록 변경
    PreparingPhase의 EnterPhase에서 호출하면, 로딩씬에서 호출되버리는 이슈로 인해..

--- Assets/02. Scripts/02. Phase/Phases/PreparingPhase.cs
+++ Assets/02. Scripts/02. Phase/Phases/PreparingPhase.cs
@@ -11,11 +11,10 @@ public class PreparingPhase : BasePhase
     public override void EnterPhase()
     {
         base.EnterPhase();
-        if(NetworkServer.active)
+        if (NetworkServer.active)
         {
             PhaseManager.Instance.ResetDeathCount();
         }
-        AudioManager.Instance.PlaySFX(EPhaseAudioType.EnterPreparingPhase);
     }
 
     public override void ExitPhase()

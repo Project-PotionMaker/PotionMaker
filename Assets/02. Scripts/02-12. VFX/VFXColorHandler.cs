65ce99c2cc2d305ebaaa736c9a7645b6ff014877
wooing26 <104512612+wooing26@users.noreply.github.com>
2025-08-18 16:36:05 +0900

    Feat | Ping 색 변경 구현

---
+++ Assets/02. Scripts/02-12. VFX/VFXColorHandler.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXColorHandler : MonoBehaviour
+{
+    [Header("플레이어 별 핑 색깔")]
+    [SerializeField]
+    private List<Color> _pingColor;
+    
+    private ParticleSystem[] _particleSystems;
+
+    private void Awake()
+    {
+        // 자신 + 모든 자식의 ParticleSystem 가져오기
+        _particleSystems = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    public void ChangeVFXColor(int playerIndex)
+    {
+        if (playerIndex < 0 || playerIndex >= _particleSystems.Length)
+        {
+            return;
+        }
+
+        foreach(ParticleSystem particleSystem in _particleSystems)
+        {
+            var main = particleSystem.main;
+            main.startColor = _pingColor[playerIndex];
+        }
+    }
+}

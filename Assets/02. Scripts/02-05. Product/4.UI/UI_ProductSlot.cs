b7e9f631a3db004ea64c5f6e25e5fc7492cf3518
wooing26 <104512612+wooing26@users.noreply.github.com>
2025-08-19 23:34:37 +0900

    Feat | UI Market Button OnClick 메서드 RemoveAll 수정

--- Assets/02. Scripts/02-05. Product/4.UI/UI_ProductSlot.cs
+++ Assets/02. Scripts/02-05. Product/4.UI/UI_ProductSlot.cs
@@ -1,12 +1,12 @@
 using System;
-using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class UI_ProductSlot:MonoBehaviour
 {
-    public event Action<ProductDTO> OnSlotClicked;
+    public event Action<ProductData> OnSlotClicked;
 
     [SerializeField]
     private Image _productImage;
@@ -20,19 +20,36 @@ public class UI_ProductSlot:MonoBehaviour
     [SerializeField]
     private CanvasGroup _lockPanel;
 
+    private ProductData _productData;
+
+    private void Start()
+    {
+        _slotButton.onClick.AddListener(OnClickSlotButtion);
+    }
+
     public void Refresh(ProductDTO productDTO)
     {
         gameObject.SetActive(false);
-        Sprite productSprite = ProductImageHelper.GetProductSprite(productDTO);
+        _productData = productDTO.Data;
+
+        Sprite productSprite = ProductImageHelper.GetProductSprite(_productData);
         _productImage.sprite = productSprite;
         _productNameTextUI.text = productDTO.Data.Name;
         _productPriceTextUI.text = productDTO.Data.Price.ToString("N0");
-        _slotButton.onClick.RemoveAllListeners();
-        _slotButton.onClick.AddListener(() => OnSlotClicked?.Invoke(productDTO));
 
         _lockPanel.alpha = productDTO.IsUnlocked ? 0 : 1;
         _slotButton.enabled = productDTO.IsUnlocked;
 
         gameObject.SetActive(true);
+    }
+
+    private void OnClickSlotButtion()
+    {
+        if (_productData == null)
+        {
+            return;
+        }
+
+        OnSlotClicked?.Invoke(_productData);
     }
 }

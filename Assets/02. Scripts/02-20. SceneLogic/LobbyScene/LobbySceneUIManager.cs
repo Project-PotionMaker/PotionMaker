45b342923a6a54d54a5772dd2ef8783ad6de50be
wooing26 <104512612+wooing26@users.noreply.github.com>
2025-08-20 10:21:49 +0900

    Feat | Scene Button 연결 및 UI Input System 설정

---
+++ Assets/02. Scripts/02-20. SceneLogic/LobbyScene/LobbySceneUIManager.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbySceneUIManager : MonoBehaviourSingleton<LobbySceneUIManager>
+{
+    private Stack<UI_FirstButtonSelector>
+}
